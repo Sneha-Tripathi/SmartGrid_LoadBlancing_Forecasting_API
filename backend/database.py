0d8deb1b2c1e64b9a37d3bce9b2211ba6a9d7c90
anjali-prog7 <anjalitiwari5810@gmail.com>
2026-07-15 08:36:57 +0530

    Day 1: Setup PostgreSQL, SQLAlchemy and database connection

---
+++ backend/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+DATABASE_URL = "postgresql://postgres:Postgres%40123@localhost:5432/smart_grid"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
