05f64887b7fd80b13f7036e0ca1ea72f4790187e
anjali-prog7 <anjalitiwari5810@gmail.com>
2026-07-21 11:14:16 +0530

    Day 5: Added environment variables and project configuration

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,22 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-DATABASE_URL = "postgresql://postgres:Postgres%40123@localhost:5432/smart_grid"
+
+# Load .env file
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 engine = create_engine(DATABASE_URL)
 
